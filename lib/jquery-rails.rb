4b1462779dfae29fdfe2774f9276141148cb74f6
Andre Arko <andre@arko.net>
2010-08-18 01:45:25 -0700

    Fix defaults overriding to actually work

--- lib/jquery-rails.rb
+++ lib/jquery-rails.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
     class Railtie < ::Rails::Railtie
-      config.before_configuration do
         if ::Rails.root.join("javascripts/jquery-ui.min.js").exist?
+      config.before_initialize do
           config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails)
         else
           config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)
