d4d1cedb782932ae8abd2ede38374ae3cbdf0d76
Andre Arko <andre@arko.net>
2010-08-18 01:45:42 -0700

    Fix jQuery-UI checking to actually work

--- lib/jquery-rails.rb
+++ lib/jquery-rails.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
     class Railtie < ::Rails::Railtie
-        if ::Rails.root.join("javascripts/jquery-ui.min.js").exist?
       config.before_initialize do
+        if ::Rails.root.join("public/javascripts/jquery-ui.min.js").exist?
           config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails)
         else
           config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)
