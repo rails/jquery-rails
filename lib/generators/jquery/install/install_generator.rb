ebe25a9631eb30f35ad0890316fee7fc79f08a03
Robert Djurasaj <robert.djurasaj@gmail.com>
2010-08-30 12:54:03 -0700

    Add possibility to install specific versions of JQuery

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -1,8 +1,11 @@
 module Jquery
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
-      desc "This generator downloads and installs jQuery 1.4.2, jQuery-ujs HEAD, and (optionally) jQuery UI 1.8.4"
+      desc "This generator downloads and installs jQuery, jQuery-ujs HEAD, and (optionally) jQuery UI 1.8.4"
       class_option :ui, :type => :boolean, :default => false, :desc => "Indicates when to Include JQueryUI (minified version; source: Google Libraries API)"
+      class_option :version, :type => :string, :default => "1.4.2", :desc => "Indicates which version of JQuery to fetch (source: Google Libraries API)"
+      @@versions = %w( 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
+
 
       def remove_prototype
         %w(controls.js dragdrop.js effects.js prototype.js).each do |js|
@@ -12,8 +15,15 @@ module Jquery
 
       def download_jquery
         # Downloading latest jQuery
-        get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js", "public/javascripts/jquery.min.js"
-        get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js", "public/javascripts/jquery.js"
+        if @@versions.include?(options.version)
+          puts "Fetching the version #{options.version} of JQuery!"
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.min.js", "public/javascripts/jquery.min.js"
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.js", "public/javascripts/jquery.js"
+        else
+          puts "Version #{options.version} is not a valid version; fetching the #{@@versions[0]} (default) version of JQuery!"
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.min.js", "public/javascripts/jquery.min.js"
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.js", "public/javascripts/jquery.js"
+        end
 
         # Downloading latest jQueryUI minified
         get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js" if options.ui?
