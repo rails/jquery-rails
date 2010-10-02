9aa7ca6f99381b828b9cf7ba82c020f2710bafe0
Andre Arko <andre@arko.net>
2010-10-02 11:11:35 -0700

    stop using minified js in development mode

--- lib/jquery-rails.rb
+++ lib/jquery-rails.rb
@@ -3,10 +3,12 @@ module Jquery
     class Railtie < ::Rails::Railtie
       config.before_configuration do
         if ::Rails.root.join("public/javascripts/jquery-ui.min.js").exist?
-          config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails)
+          jq_defaults = %w(jquery jquery-ui)
+          jq_defaults.map!{|a| a + ".min" } unless ::Rails.env.development?
         else
-          config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)
+          jq_defaults = ::Rails.env.development? ? %w(jquery) : %w(jquery.min)
         end
+        config.action_view.javascript_expansions[:defaults] = jq_defaults + %w(rails)
       end
     end
   end
--- spec/jquery-rails_spec.rb
+++ spec/jquery-rails_spec.rb
@@ -1,15 +1,19 @@
 describe "jQuery-Rails" do
-  def get_js_defaults(name)
+  def get_js_defaults(name, env = "production")
     dir = File.expand_path("../support/#{name}_app", __FILE__)
     Dir.chdir(dir) do
       `bundle install --local`
-      `rails runner 'puts Rails.application.config.action_view.
+      `rails runner -e #{env} 'puts Rails.application.config.action_view.
         javascript_expansions[:defaults].inspect'`.chomp
     end
   end
 
   it "changes the default javascript expansion" do
     get_js_defaults("default").should == ["jquery.min", "rails"].inspect
+  end
+
+  it "uses non-minified js in development" do
+    get_js_defaults("default", "development").should == %w(jquery rails).inspect
   end
 
   it "changes allows overriding the javascript expansion" do
