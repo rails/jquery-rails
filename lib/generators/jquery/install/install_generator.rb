3a3adcf93997b6bf2450becd215cd71a0ca30ff4
Andre Arko <andre@arko.net>
2010-10-02 11:07:04 -0700

    Clean up messages a bit

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -2,8 +2,8 @@ module Jquery
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       desc "This generator downloads and installs jQuery, jQuery-ujs HEAD, and (optionally) jQuery UI 1.8.4"
-      class_option :ui, :type => :boolean, :default => false, :desc => "Indicates when to Include JQueryUI (minified version; source: Google Libraries API)"
-      class_option :version, :type => :string, :default => "1.4.2", :desc => "Indicates which version of JQuery to fetch (source: Google Libraries API)"
+      class_option :ui, :type => :boolean, :default => false, :desc => "Whether to Include JQueryUI"
+      class_option :version, :type => :string, :default => "1.4.2", :desc => "Which version of JQuery to fetch"
       @@versions = %w( 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
 
 
@@ -16,11 +16,11 @@ module Jquery
       def download_jquery
         # Downloading latest jQuery
         if @@versions.include?(options.version)
-          puts "Fetching the version #{options.version} of JQuery!"
+          puts "Fetching JQuery version #{options.version}!"
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.min.js", "public/javascripts/jquery.min.js"
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.js", "public/javascripts/jquery.js"
         else
-          puts "Version #{options.version} is not a valid version; fetching the #{@@versions[0]} (default) version of JQuery!"
+          puts "JQuery #{options.version} is invalid; fetching #{@@versions[0]} instead."
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.min.js", "public/javascripts/jquery.min.js"
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.js", "public/javascripts/jquery.js"
         end
