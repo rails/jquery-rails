41cc61d69212aec4171c1365f0f1a5e35f992b2d
Andre Arko <andre@arko.net>
2010-10-02 11:18:11 -0700

    JQuery-ujs recommends 1.4.1 because of http://jsbin.com/uboxu3/7/

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -3,7 +3,7 @@ module Jquery
     class InstallGenerator < ::Rails::Generators::Base
       desc "This generator downloads and installs jQuery, jQuery-ujs HEAD, and (optionally) jQuery UI 1.8.4"
       class_option :ui, :type => :boolean, :default => false, :desc => "Whether to Include JQueryUI"
-      class_option :version, :type => :string, :default => "1.4.2", :desc => "Which version of JQuery to fetch"
+      class_option :version, :type => :string, :default => "1.4.1", :desc => "Which version of JQuery to fetch"
       @@versions = %w( 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
 
 
@@ -20,9 +20,9 @@ module Jquery
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.min.js", "public/javascripts/jquery.min.js"
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.js", "public/javascripts/jquery.js"
         else
-          puts "JQuery #{options.version} is invalid; fetching #{@@versions[0]} instead."
-          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.min.js", "public/javascripts/jquery.min.js"
-          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[0]}/jquery.js", "public/javascripts/jquery.js"
+          puts "JQuery #{options.version} is invalid; fetching #{@@versions[1]} instead."
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[1]}/jquery.min.js", "public/javascripts/jquery.min.js"
+          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[1]}/jquery.js", "public/javascripts/jquery.js"
         end
 
         # Downloading latest jQueryUI minified
--- lib/jquery-rails/version.rb
+++ lib/jquery-rails/version.rb
@@ -1,5 +1,5 @@
 module Jquery
   module Rails
-    VERSION = "0.2"
+    VERSION = "0.2.1"
   end
 end
