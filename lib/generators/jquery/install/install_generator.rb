b820540b72cef8ca275fcc3fcdf20759ed66ae78
Victor Costan <costan@gmail.com>
2010-10-16 16:34:06 -0400

    Slightly better version selection for jquery and jquery UI.

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -4,7 +4,7 @@ module Jquery
       desc "This generator downloads and installs jQuery, jQuery-ujs HEAD, and (optionally) jQuery UI 1.8.4"
       class_option :ui, :type => :boolean, :default => false, :desc => "Whether to Include JQueryUI"
       class_option :version, :type => :string, :default => "1.4.1", :desc => "Which version of JQuery to fetch"
-      @@versions = %w( 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
+      @@versions = %w( 1.4.3 1.4.2 1.4.1 1.4.0 1.3.2 1.3.1 1.3.0 1.2.6 )
 
 
       def remove_prototype
@@ -20,14 +20,22 @@ module Jquery
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.min.js", "public/javascripts/jquery.min.js"
           get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.js", "public/javascripts/jquery.js"
         else
-          puts "JQuery #{options.version} is invalid; fetching #{@@versions[1]} instead."
-          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[1]}/jquery.min.js", "public/javascripts/jquery.min.js"
-          get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[1]}/jquery.js", "public/javascripts/jquery.js"
+          # Try to get it anyway
+          begin
+            puts "Trying to fetch JQuery version #{options.version}, even though I don't know about it.'"
+            get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.min.js", "public/javascripts/jquery.min.js"
+            get "http://ajax.googleapis.com/ajax/libs/jquery/#{options.version}/jquery.js", "public/javascripts/jquery.js"
+            puts "Success"
+          rescue
+            puts "JQuery #{options.version} is invalid; fetching #{@@versions[2]} instead."
+            get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[2]}/jquery.min.js", "public/javascripts/jquery.min.js"
+            get "http://ajax.googleapis.com/ajax/libs/jquery/#{@@versions[2]}/jquery.js", "public/javascripts/jquery.js"
+          end
         end
 
         # Downloading latest jQueryUI minified
-        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js" if options.ui?
-        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.js", "public/javascripts/jquery-ui.js" if options.ui?
+        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js" if options.ui?
+        get "http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.js", "public/javascripts/jquery-ui.js" if options.ui?
       end
 
       def download_ujs_driver
