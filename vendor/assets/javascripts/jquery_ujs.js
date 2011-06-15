c7da6fe5988970a8b4b08982739467d2469c1717
Steve Schwartz <steve@alfajango.com>
2011-06-15 19:32:46 -0400

    Updated jquery-ujs to add cross-domain request fix to exclude CSRF header insertion.

--- vendor/assets/javascripts/jquery_ujs.js
+++ vendor/assets/javascripts/jquery_ujs.js
@@ -95,6 +95,7 @@
     // Submits "remote" forms and links with ajax
     handleRemote: function(element) {
       var method, url, data,
+        crossDomain = element.data('cross-domain') || null,
         dataType = element.data('type') || ($.ajaxSettings && $.ajaxSettings.dataType);
 
       if (rails.fire(element, 'ajax:before')) {
@@ -116,7 +117,7 @@
        }
 
         rails.ajax({
-          url: url, type: method || 'GET', data: data, dataType: dataType,
+          url: url, type: method || 'GET', data: data, dataType: dataType, crossDomain: crossDomain,
           // stopping the "ajax:beforeSend" event will cancel the ajax request
           beforeSend: function(xhr, settings) {
             if (settings.dataType === undefined) {
@@ -244,9 +245,9 @@
 
   // ajaxPrefilter is a jQuery 1.5 feature
   if ('ajaxPrefilter' in $) {
-    $.ajaxPrefilter(function(options, originalOptions, xhr){ rails.CSRFProtection(xhr); });
+    $.ajaxPrefilter(function(options, originalOptions, xhr){ if ( !options.crossDomain ) { rails.CSRFProtection(xhr); }});
   } else {
-    $(document).ajaxSend(function(e, xhr){ rails.CSRFProtection(xhr); });
+    $(document).ajaxSend(function(e, xhr, options){ if ( !options.crossDomain ) { rails.CSRFProtection(xhr); }});
   }
 
   $(rails.linkClickSelector).live('click.rails', function(e) {
