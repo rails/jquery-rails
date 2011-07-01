dd48a400f045ddb60c55dad565f394ccca71e5b7
Franco Catena <francocatena@gmail.com>
2011-07-01 11:16:32 -0300

    Version number updated in version.rb

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
     VERSION = "1.0.12"
-    JQUERY_VERSION = "1.6.1"
-    JQUERY_UI_VERSION  = "1.8.12"
+    JQUERY_VERSION = "1.6.2"
+    JQUERY_UI_VERSION  = "1.8.14"
     JQUERY_UJS_VERSION = "0e742688c633bda274f8a09bf3248ffe33ecaf92"
   end
 end
