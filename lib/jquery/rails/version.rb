976da50bf6671a916bcac6ea1f586918bdf0090e
Steve Schwartz <steve@alfajango.com>
2011-09-08 14:00:44 -0400

    Updated to latest jquery-ujs

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -3,6 +3,6 @@ module Jquery
     VERSION = "1.0.13"
     JQUERY_VERSION = "1.6.2"
     JQUERY_UI_VERSION  = "1.8.14"
-    JQUERY_UJS_VERSION = "cd619df9f0daad3303aacd4f992fff19158b1e5d"
+    JQUERY_UJS_VERSION = "c00a9726837756ea53404ca7e8b0f12df19a6496"
   end
 end
