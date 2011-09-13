2fdbeef29e7a8faa0eb53017c7425bf318d890fc
Sergey Fedorov <strech_ftf@mail.ru>
2011-09-13 15:05:26 +0600

    Default accept extensions change

--- vendor/assets/javascripts/jquery_ujs.js
+++ vendor/assets/javascripts/jquery_ujs.js
@@ -133,7 +133,7 @@
           // stopping the "ajax:beforeSend" event will cancel the ajax request
           beforeSend: function(xhr, settings) {
             if (settings.dataType === undefined) {
-              xhr.setRequestHeader('accept', '*/*;q=0.5, ' + settings.accepts._default);
+              xhr.setRequestHeader('accept', '*/*;q=0.5, ' + settings.accepts.*);
             }
             return rails.fire(element, 'ajax:beforeSend', [xhr, settings]);
           },
