49b002548e9f7737c2556e8582b54c2c8e1838ab
Steve Schwartz <steve@alfajango.com>
2011-10-12 17:24:02 -0400

    Upgraded to jquery 1.6.4 and jquery-ui 1.8.16

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
     VERSION = "1.0.15"
-    JQUERY_VERSION = "1.6.2"
-    JQUERY_UI_VERSION  = "1.8.14"
+    JQUERY_VERSION = "1.6.4"
+    JQUERY_UI_VERSION  = "1.8.16"
     JQUERY_UJS_VERSION = "acd1a149fb715710c22e56ee4c50a9c6ab840709"
   end
 end
