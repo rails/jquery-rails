d03c90e0b3b9239b474c59c2f8fc835fc558fdc4
Evan Walsh <evan@nothingconcept.com>
2011-11-03 17:27:34 -0400

    Update to jQuery 1.7.0

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,7 +1,7 @@
 module Jquery
   module Rails
-    VERSION = "1.0.15"
-    JQUERY_VERSION = "1.6.4"
+    VERSION = "1.0.16"
+    JQUERY_VERSION = "1.7.0"
     JQUERY_UI_VERSION  = "1.8.16"
     JQUERY_UJS_VERSION = "acd1a149fb715710c22e56ee4c50a9c6ab840709"
   end
