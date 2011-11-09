f036cc4a1b683ed099234af58d64b23bf8302a33
Steve Schwartz <steve@alfajango.com>
2011-11-09 13:13:43 -0500

    Added generator back for Rails 3.1 apps with asset pipeline turned off. Closes #28.

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -1,6 +1,7 @@
 require 'rails'
 
-if ::Rails.version < "3.1"
+# Supply generator for Rails 3.0.x or if asset pipeline is not enabled
+if ::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
   module Jquery
     module Generators
       class InstallGenerator < ::Rails::Generators::Base
@@ -45,9 +46,11 @@ else
         desc "Just show instructions so people will know what to do when mistakenly using generator for Rails 3.1 apps"
 
         def do_nothing
-          say_status("deprecated", "You are using Rails 3.1, so this generator is no longer needed. The necessary files are already in your asset pipeline.")
+          say_status("deprecated", "You are using Rails 3.1 with the asset pipeline enabled, so this generator is not needed.")
+          say_status("", "The necessary files are already in your asset pipeline.")
           say_status("", "Just add `//= require jquery` and `//= require jquery_ujs` to your app/assets/javascripts/application.js")
           say_status("", "If you upgraded your app from Rails 3.0 and still have jquery.js, rails.js, or jquery_ujs.js in your javascripts, be sure to remove them.")
+          say_status("", "If you do not want the asset pipeline enabled, you may turn it off in application.rb and re-run this generator.")
           # ok, nothing
         end
       end
