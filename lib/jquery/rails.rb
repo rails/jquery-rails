84a3670769987cd20f5c6b19171cdc92905c7d43
José Valim <jose.valim@gmail.com>
2011-12-20 17:38:43 +0100

    Update API.

--- lib/jquery/rails.rb
+++ lib/jquery/rails.rb
@@ -2,11 +2,7 @@ module Jquery
   module Rails
     PROTOTYPE_JS = %w{prototype effects dragdrop controls}
 
-    if ::Rails.version < "3.1"
-      require 'jquery/rails/railtie'
-    else
-      require 'jquery/rails/engine'
-    end
+    require 'jquery/rails/engine'
     require 'jquery/rails/version'
   end
 end
--- lib/jquery/rails/engine.rb
+++ lib/jquery/rails/engine.rb
@@ -1,12 +1,8 @@
-# Configure Rails 3.1 to have assert_select_jquery() in tests
+require "jquery/assert_select" if ::Rails.env.test?
+
 module Jquery
   module Rails
-
     class Engine < ::Rails::Engine
-      config.before_configuration do
-        require "jquery/assert_select" if ::Rails.env.test?
-      end
     end
-
   end
 end
