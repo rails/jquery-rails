74997e62ab10698995f02fd75c2f18208d19a6f3
José Valim <jose.valim@gmail.com>
2011-12-20 17:39:13 +0100

    Release 2.0.0 with support for Rails 3.2+ only.

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,6 +1,6 @@
 module Jquery
   module Rails
-    VERSION = "1.0.19"
+    VERSION = "2.0.0"
     JQUERY_VERSION = "1.7.1"
     JQUERY_UI_VERSION  = "1.8.16"
     JQUERY_UJS_VERSION = "82292010fb1743f038ab72b1f1e994e91be3eda3"
