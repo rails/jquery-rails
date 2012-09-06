ec0bf15eb8f4f142b050ffebe368cf934d30a0d5
Steve Schwartz <steve@alfajango.com>
2012-09-06 19:40:42 -0400

    Updated to jquery 1.8.1

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,7 +1,7 @@
 module Jquery
   module Rails
     VERSION = "2.1.1"
-    JQUERY_VERSION = "1.8.0"
+    JQUERY_VERSION = "1.8.1"
     JQUERY_UI_VERSION  = "1.8.23"
     JQUERY_UJS_VERSION = "bddff6a677edc54f00e48bde740b0b22d68deef6"
   end
