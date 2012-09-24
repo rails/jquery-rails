c3d6a3750a296b0b3da14f5e38f758164068c385
Maksim Berjoza <torbjon@gmail.com>
2012-09-24 13:14:16 +0300

    missed version change in version.rb

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,6 +1,6 @@
 module Jquery
   module Rails
-    VERSION = "2.1.3"
+    VERSION = "2.1.4"
     JQUERY_VERSION = "1.8.2"
     JQUERY_UI_VERSION  = "1.8.23"
     JQUERY_UJS_VERSION = "bddff6a677edc54f00e48bde740b0b22d68deef6"
