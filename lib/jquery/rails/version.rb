b8ca8b8dbb7c43eb90b6018118b3dfe4552a8e57
Josef Sin <josef.sin@gmail.com>
2012-10-10 10:40:29 +0200

    Updated to jQuery UI 1.9.0

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
-    VERSION = "2.1.3"
+    VERSION = "2.2.0"
     JQUERY_VERSION = "1.8.2"
-    JQUERY_UI_VERSION  = "1.8.23"
+    JQUERY_UI_VERSION  = "1.9.0"
     JQUERY_UJS_VERSION = "bddff6a677edc54f00e48bde740b0b22d68deef6"
   end
 end
