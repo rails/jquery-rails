8b1a118720ac5cab5691f75f67301d2915b01e4e
Steve Schwartz <steve@alfajango.com>
2013-01-19 12:12:51 -0500

    Version 2.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
-## 2.2.0 (16 January 2012)
+## 2.2.0 (19 January 2012)
 
   - Updated to jQuery 1.9.0
+  - Updated to latest jquery-ujs
 
 ## 2.1.4 (26 November 2012)
 
--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,6 +1,6 @@
 module Jquery
   module Rails
-    VERSION = "2.1.4"
+    VERSION = "2.2.0"
     JQUERY_VERSION = "1.8.3"
     JQUERY_UI_VERSION  = "1.9.2"
     JQUERY_UJS_VERSION = "bddff6a677edc54f00e48bde740b0b22d68deef6"
