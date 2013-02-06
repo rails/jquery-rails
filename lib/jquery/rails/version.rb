b2c4e626a891ca97553b1766622381d2dd8fd8e2
Timo Uhlmann <t.uhlmann@zeroseven.de>
2013-02-06 13:46:49 +0100

    Updated jQuery Migrate version

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -2,7 +2,7 @@ module Jquery
   module Rails
     VERSION = "2.2.0"
     JQUERY_VERSION = "1.9.0"
-    JQUERY_MIGRATE_VERSION = "1.0.0"
+    JQUERY_MIGRATE_VERSION = "1.1.0"
     JQUERY_UI_VERSION  = "1.9.2"
     JQUERY_UJS_VERSION = "bddff6a677edc54f00e48bde740b0b22d68deef6"
   end
