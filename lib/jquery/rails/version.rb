b27c0d2181b540af84356bdec8b3e5267916d21d
Steve Schwartz <steve@alfajango.com>
2013-06-07 22:19:48 -0400

    Updated to jQuery 1.10.1.

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,7 +1,7 @@
 module Jquery
   module Rails
     VERSION = "3.0.0"
-    JQUERY_VERSION = "1.10.0"
+    JQUERY_VERSION = "1.10.1"
     JQUERY_UJS_VERSION = "87587d476054e9869865f53c1b78cb8164619a05"
   end
 end
