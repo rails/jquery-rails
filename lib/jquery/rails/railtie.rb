8f5ebba3b3a1444e3a20ecd67a99f1e1060633ce
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2013-12-23 14:50:19 +0100

    This is no longer needed and was fixed in rails itself
    Revert "Rails 4 compatibility: require action_view"
    
    This reverts commit 6fc528253c453a133bbad8eb72a8e748926e7cc7.

--- lib/jquery/rails/railtie.rb
+++ lib/jquery/rails/railtie.rb
@@ -1,7 +1,5 @@
 # Used to ensure that Rails 3.0.x, as well as Rails >= 3.1 with asset pipeline disabled
 # get the minified version of the scripts included into the layout in production.
-require 'action_view/railtie'
-
 module Jquery
   module Rails
     class Railtie < ::Rails::Railtie
