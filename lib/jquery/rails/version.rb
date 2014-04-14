44b441152c202d5459119af63bd6a7fc6c683b2d
Alexei Curguzchin <alexei.lexx@gmail.com>
2014-04-14 18:55:24 +0300

    Updated version to 3.1.1

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,6 +1,6 @@
 module Jquery
   module Rails
-    VERSION = "3.1.0"
+    VERSION = "3.1.1"
     JQUERY_VERSION = "1.11.0"
     JQUERY_UJS_VERSION = "f160fa2f4615f93e1a0d75e49de59d19c18c8728"
   end
