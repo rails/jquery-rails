796b0c22423919644095f07804f294d6da9b48e9
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-11-25 22:04:39 -0200

    Upgrade jquery_ujs to 1.0.2

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -3,6 +3,6 @@ module Jquery
     VERSION = "4.0.0.beta2"
     JQUERY_VERSION = "1.11.1"
     JQUERY_2_VERSION = "2.1.1"
-    JQUERY_UJS_VERSION = "1.0.1"
+    JQUERY_UJS_VERSION = "1.0.2"
   end
 end
