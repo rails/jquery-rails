ff93f91b279a760472e459e8e4ad6cdf1cc5bcf4
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-12-29 18:15:08 -0300

    Upgrade jQuery versions
    Fixes #183

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
-    VERSION = "4.0.2"
-    JQUERY_VERSION = "1.11.1"
-    JQUERY_2_VERSION = "2.1.1"
-    JQUERY_UJS_VERSION = "1.0.2"
+    VERSION = "4.0.3"
+    JQUERY_VERSION = "1.11.2"
+    JQUERY_2_VERSION = "2.1.3"
+    JQUERY_UJS_VERSION = "1.0.3"
   end
 end
