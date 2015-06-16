b265b4669dc1d59c489eda8f25a3a55cd94d11b8
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-06-16 13:04:41 -0300

    Release 4.0.4

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Rails
-    VERSION = "4.0.3"
+    VERSION = "4.0.4"
     JQUERY_VERSION = "1.11.2"
     JQUERY_2_VERSION = "2.1.3"
-    JQUERY_UJS_VERSION = "1.0.3"
+    JQUERY_UJS_VERSION = "1.0.4"
   end
 end
