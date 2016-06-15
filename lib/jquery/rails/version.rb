7f98228f3547b56266ac3ab665765090dbd35166
Rob Cameron <cannikinn@gmail.com>
2016-06-15 10:19:36 -0700

    Updates VERSION and adds JQUERY_3_VERSION

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,9 @@
 module Jquery
   module Rails
-    VERSION = "4.1.1"
+    VERSION = "5.0.0"
     JQUERY_VERSION = "1.12.1"
     JQUERY_2_VERSION = "2.2.1"
+    JQUERY_3_VERSION = "3.0.0"
     JQUERY_UJS_VERSION = "1.2.1"
   end
 end
