bc26c46e9d46fa7410e21833e802e82a1c8458cb
MSathieu <MSathieu@users.noreply.github.com>
2016-07-28 09:47:16 +0200

    Update JQuery3

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -3,7 +3,7 @@ module Jquery
     VERSION = "4.2.0"
     JQUERY_VERSION = "1.12.4"
     JQUERY_2_VERSION = "2.2.4"
-    JQUERY_3_VERSION = "3.0.0"
+    JQUERY_3_VERSION = "3.1.0"
     JQUERY_UJS_VERSION = "1.2.1"
   end
 end
