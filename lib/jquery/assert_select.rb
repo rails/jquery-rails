9ddcbf13f229d18f0bfc137cd190ca0465111cb2
Juan Pazmino <juankpro@outlook.com>
2017-05-21 23:07:51 -0500

    Add support for nested jquery assertions

--- lib/jquery/assert_select.rb
+++ lib/jquery/assert_select.rb
@@ -32,13 +32,20 @@ module Rails::Dom::Testing::Assertions::SelectorAssertions
   # === Examples
   #
   # # asserts that the #notice element is hidden
-  # assert_select :hide, '#notice'
+  # assert_select_jquery :hide, '#notice'
   #
   # # asserts that the #cart element is shown with a blind parameter
-  # assert_select :show, :blind, '#cart'
+  # assert_select_jquery :show, :blind, '#cart'
+  #
+  # # asserts that the #cart content contains #current_item after a blind effect
+  # assert_select_jquery :hide, :blind, '#cart' do
+  #   assert_select_jquery :html, '#cart' do
+  #     assert_select '#current_item'
+  #   end
+  # end
   #
   # # asserts that #cart content contains a #current_item
-  # assert_select :html, '#cart' do
+  # assert_select_jquery :html, '#cart' do
   #   assert_select '#current_item'
   # end
   #
@@ -47,6 +54,8 @@ module Rails::Dom::Testing::Assertions::SelectorAssertions
   #   assert_select '.product'
   # end
 
+  PATTERN_FUNCTION_DECL = "function\\s*\\(.*\\)\\s*"
+  PATTERN_CALLBACK = ".*#{PATTERN_FUNCTION_DECL}(\\{(?:[^{}]|\\g<1>)+\\})"
   PATTERN_HTML  = "['\"]((\\\\\"|\\\\'|[^\"'])*)['\"]"
   PATTERN_UNICODE_ESCAPED_CHAR = /\\u([0-9a-zA-Z]{4})/
   SKELETAL_PATTERN = "(?:jQuery|\\$)\\(%s\\)\\.%s\\(%s\\);"
@@ -58,9 +67,10 @@ module Rails::Dom::Testing::Assertions::SelectorAssertions
 
     target_pattern   = "['\"]#{id || '.*'}['\"]"
     method_pattern   = "#{jquery_method || '\\w+'}"
-    argument_pattern = jquery_opt ? "['\"]#{jquery_opt}['\"].*" : PATTERN_HTML
+    argument_pattern = jquery_opt ? "['\"]#{jquery_opt}['\"](?:#{PATTERN_CALLBACK}|.*)" : PATTERN_HTML
 
     # $("#id").show('blind', 1000);
+    # $("#id").show('blind', 1000, function() { ... });
     # $("#id").html("<div>something</div>");
     # $("#id").replaceWith("<div>something</div>");
     target_as_receiver_pattern = SKELETAL_PATTERN % [target_pattern, method_pattern, argument_pattern]
@@ -91,29 +101,44 @@ module Rails::Dom::Testing::Assertions::SelectorAssertions
 
     if block_given?
       @selected ||= nil
-      fragments = Nokogiri::HTML::Document.new.fragment
 
-      if matched_pattern
+      if jquery_opt
+        code_blocks = []
         response.body.scan(Regexp.new(matched_pattern)).each do |match|
-          flunk 'This function can\'t have HTML argument' if match.is_a?(String)
+          flunk 'This function can\'t have a callback' if match.first.nil?
+          code_blocks << match.first
+        end
 
-          doc = Nokogiri::HTML::DocumentFragment.parse(unescape_js(match.first))
-          doc.children.each do |child|
-            fragments << child if child.element?
+        yield_with_content(code_blocks.join("\n"), &block)
+      else
+        fragments = Nokogiri::HTML::Document.new.fragment
+
+        if matched_pattern
+          response.body.scan(Regexp.new(matched_pattern)).each do |match|
+            flunk 'This function can\'t have HTML argument' if match.is_a?(String)
+
+            doc = Nokogiri::HTML::DocumentFragment.parse(unescape_js(match.first))
+            doc.children.each do |child|
+              fragments << child if child.element?
+            end
           end
         end
-      end
 
-      begin
-        in_scope, @selected = @selected, fragments
-        yield
-      ensure
-        @selected = in_scope
+        yield_with_content(fragments, &block)
       end
     end
   end
 
   private
+
+    def yield_with_content(content)
+      begin
+        in_scope, @selected = @selected, content
+        yield
+      ensure
+        @selected = in_scope
+      end
+    end
 
     # Unescapes a JS string.
     def unescape_js(js_string)
--- test/assert_select_jquery_test.rb
+++ test/assert_select_jquery_test.rb
@@ -1,6 +1,5 @@
 require_relative 'test_helper'
 require_relative '../lib/jquery/assert_select'
-
 class AssertSelectJQueryTest < ActiveSupport::TestCase
   include Rails::Dom::Testing::Assertions::SelectorAssertions
   attr_reader :response
@@ -19,6 +18,16 @@ class AssertSelectJQueryTest < ActiveSupport::TestCase
     $("#cart tr:not(.total_line) > *").remove();
     $("[href|=\"val\"][href$=\"val\"][href^=\"val\"]").remove();
     $("tr + td, li").remove();
+    $("#card").hide("blind", 100, function() {
+      $("#content").html('<div><p>something</p></div>');
+    });
+    $("#card").hide("bounce", 100, function() {
+      $("#content").html('<div><p>animating hide</p></div>');
+
+      $("#card").show("bounce", 100, function() {
+        $("#content").html('<div><p>animating show</p></div>');
+      });
+    });
   JS
 
   setup do
@@ -38,6 +47,22 @@ class AssertSelectJQueryTest < ActiveSupport::TestCase
       assert_select_jquery :remove, "#cart tr:not(.total_line) > *"
       assert_select_jquery :remove, "[href|=\"val\"][href$=\"val\"][href^=\"val\"]"
       assert_select_jquery :remove, "tr + td, li"
+
+      # These allow to assert correct execution order in addition to simple execution assertion
+      assert_select_jquery :hide, :bounce, '#card' do
+        # assert multiple jquery calls within different callbacks
+        assert_select_jquery :html, '#content' do
+          assert_select 'p', 'something'
+          assert_select 'p', 'animating hide'
+        end
+
+        # assert callback-nested jquery calls
+        assert_select_jquery :show, :bounce, '#card' do
+          assert_select_jquery :html, '#content' do
+            assert_select 'p', 'animating show'
+          end
+        end
+      end
     end
 
     assert_raise Minitest::Assertion, "No JQuery call matches [:show, :some_wrong]" do
@@ -48,6 +73,10 @@ class AssertSelectJQueryTest < ActiveSupport::TestCase
       assert_select_jquery :html, '#id' do
         assert_select 'p', 'something else'
       end
+    end
+
+    assert_raise Minitest::Assertion, "This function can't have a callback" do
+      assert_select_jquery(:show, :blind, '#card') { }
     end
   end
 
@@ -71,6 +100,10 @@ class AssertSelectJQueryTest < ActiveSupport::TestCase
     assert_nothing_raised do
       assert_select_jquery :remove
       assert_select_jquery :hide
+    end
+
+    assert_raise Minitest::Assertion, "This function can't have HTML argument" do
+      assert_select_jquery(:remove) { }
     end
 
     assert_raise Minitest::Assertion, 'No JQuery call matches [:wrong_function]' do
