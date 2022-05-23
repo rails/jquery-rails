8cd051ac93b0f445b127c9313c8c46282e4e45bc
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-05-23 18:17:10 -0300

    Update jQuery to 3.6.0
    Closes #280
    Closes #285

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -3,7 +3,7 @@ module Jquery
     VERSION = "4.4.0"
     JQUERY_VERSION = "1.12.4"
     JQUERY_2_VERSION = "2.2.4"
-    JQUERY_3_VERSION = "3.5.1"
+    JQUERY_3_VERSION = "3.6.0"
     JQUERY_UJS_VERSION = "1.2.2"
   end
 end
