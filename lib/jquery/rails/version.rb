fb2e4c4d372ad159673b667412387cb1aacdaff5
Charl Matthee <charl@ntrippy.net>
2022-07-20 16:10:38 +0200

    Upgraded jQuery to vv3.6.0.

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,7 +1,9 @@
 module Jquery
   module Rails
-    VERSION = "3.1.5"
+    VERSION = "3.1.6-oc"
     JQUERY_VERSION = "1.12.4"
-    JQUERY_UJS_VERSION = "1.0.4"
+    JQUERY_2_VERSION = "2.2.4"
+    JQUERY_3_VERSION = "3.6.0"
+    JQUERY_UJS_VERSION = "1.2.2"
   end
 end
