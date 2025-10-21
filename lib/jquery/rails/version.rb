f7a8b76fe197ef69811ab066a3948066da5e761b
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-10-21 17:08:16 -0300

    Update jquery to v3.7.1
    https://blog.jquery.com/2023/08/28/jquery-3-7-1-released-reliable-table-row-dimensions/
    
    Closes #300

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -3,7 +3,7 @@ module Jquery
     VERSION = "4.6.0"
     JQUERY_VERSION = "1.12.4"
     JQUERY_2_VERSION = "2.2.4"
-    JQUERY_3_VERSION = "3.7.0"
+    JQUERY_3_VERSION = "3.7.1"
     JQUERY_UJS_VERSION = "1.2.3"
   end
 end
